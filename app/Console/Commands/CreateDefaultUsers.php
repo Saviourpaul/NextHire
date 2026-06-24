f1c332508a517242aaa172be09c9c899c18c89a0
saviour paul <saviourpaul24@gmail.com>
2026-06-24 14:50:56 +0100

    default users details updated

---
+++ app/Console/Commands/CreateDefaultUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateDefaultUsers extends Command
+{
+    protected $signature = 'app:create-default-users';
+
+    protected $description = 'Create default admin ';
+
+    public function handle()
+    {
+        User::updateOrCreate(
+            ['email' => 'saviourpaul24@gmail.com'],
+            [
+                'name' => 'System Administrator',
+                'password' => Hash::make('123456789'),
+                'role' => 'admin',
+            ]
+        );
+
+       
+
+        $this->info('Default users created successfully.');
+
+        return Command::SUCCESS;
+    }
+}
