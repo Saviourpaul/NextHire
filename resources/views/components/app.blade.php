fc40e0ae5e6fa276c0946b743244adc4c24b546c
saviour paul <saviourpaul24@gmail.com>
2026-07-04 17:06:43 +0100

    features blade removed

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -87,9 +87,7 @@
 								<li class="{{ request()->routeIs('services') ? 'active' : '' }}">
 									<a href="{{ route('services') }}">Services</a>
 								</li>
-								<li class="{{ request()->routeIs('features') ? 'active' : '' }}">
-									<a href="{{ route('features') }}">Features</a>
-								</li>
+								
 								<li class="{{ request()->routeIs('about') ? 'active' : '' }}">
 									<a href="{{ route('about') }}">About</a>
 								</li>
@@ -190,7 +188,6 @@
 											<ul>
 												<li><a href="{{ route('about') }}"><i class="fas fa-angle-right me-1"></i>About Us</a></li>
 												<li><a href="{{ route('services') }}"><i class="fas fa-angle-right me-1"></i>Services</a></li>
-												<li><a href="{{ route('features') }}"><i class="fas fa-angle-right me-1"></i>Features</a></li>
 												<li><a href="{{ route('jobs.public') }}"><i class="fas fa-angle-right me-1"></i>Browse Jobs</a></li>
 												<li><a href="{{ route('faq') }}"><i class="fas fa-angle-right me-1"></i>FAQ</a></li>
 											</ul>
